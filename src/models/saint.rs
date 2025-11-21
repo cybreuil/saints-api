451240c36563ad90d37d8ec7f317665039556095
Cyrille Breuil <cybreuil@gmail.com>
2025-11-21 17:07:10 +0100

    fix

--- src/models/saint.rs
+++ src/models/saint.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 pub struct Saint {
     pub id: i32,
     pub name: String,
-    pub feast_day: String, // Format "YYYY-MM-DD" ou à adapter selon besoin
+    pub feast_day: String, // Format "YYYY-MM-DD" ou à adapter
     pub description: Option<String>,
-    // Ajoute d'autres champs selon ton besoin (calendrier, missel, etc.)
+    // peut etre étendu avec d'autres champs si nécessaire
 }
