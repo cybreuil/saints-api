8079a07822336f5b2d9d15b7f2181a5eb3357608
Cyrille Breuil <cybreuil@gmail.com>
2025-11-21 16:33:38 +0100

    feat: hello

---
+++ src/main/java/com/saints/SaintsApiApplication.java
@@ -0,0 +1,12 @@
+package com.saints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintsApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/saints/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.saints.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Bonjour, API Saints prête !";
+	}
+}
