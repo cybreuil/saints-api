5ddf982c2d789014b876763bbc0bdd954df9fa06
Cyrille Breuil <cybreuil@gmail.com>
2026-05-23 17:21:53 +0200

    huge refactor

--- src/modules/saints/dto.rs
+++ src/modules/saints/dto.rs
@@ -1,15 +1,17 @@
 // use chrono::NaiveDate;
+use super::model::SaintRow;
 use serde::{Deserialize, Serialize};
-// use sqlx::FromRow;
 
-#[derive(Serialize)]
-struct SaintListResponse {
-    page: i32,
-    per_page: i32,
-    data: Vec<SaintListItem>,
+// Saints Paginated Response
+#[derive(Debug, Serialize)]
+pub struct SaintListResponse {
+    pub page: i32,
+    pub per_page: i32,
+    pub data: Vec<SaintListItem>,
 }
 
-#[derive(Serialize)]
+// Saint small struct for listing
+#[derive(Debug, Serialize)]
 pub struct SaintListItem {
     pub id: i32,
     pub slug: String,
@@ -17,7 +19,25 @@ pub struct SaintListItem {
     pub birth_year: Option<i16>,
     pub death_year: Option<i16>,
 }
+// Mapping from SaintRow to SaintListItem
+impl From<SaintRow> for SaintListItem {
+    fn from(row: SaintRow) -> Self {
+        Self {
+            id: row.id,
+            slug: row.slug,
+            default_name: row.default_name,
+            birth_year: row.birth_year,
+            death_year: row.death_year,
+        }
+    }
+}
 
+#[derive(Debug, Deserialize)]
+pub struct SaintQuery {
+    pub locale: Option<String>,
+    pub page: Option<i32>,
+    pub per_page: Option<i32>,
+}
 // #[derive(Debug, Deserialize)]
 // pub struct CreateSaint {
 //     pub slug: String,
@@ -36,19 +56,3 @@ pub struct SaintListItem {
 //     pub short_description: Option<String>,
 //     pub image_url: Option<String>,
 // }
-
-#[derive(Debug, Deserialize)]
-pub struct SaintQuery {
-    pub locale: Option<String>,
-    pub page: Option<i32>,
-    pub per_page: Option<i32>,
-}
-
-#[derive(sqlx::FromRow)]
-pub struct SaintRow {
-    pub id: i32,
-    pub slug: String,
-    pub default_name: String,
-    pub birth_year: Option<i16>,
-    pub death_year: Option<i16>,
-}
