37e26952b21595266bc942299ecae123dfa3848b
Cyrille Breuil <cybreuil@gmail.com>
2026-05-23 17:37:24 +0200

    dto direct fromRow & fix

--- src/modules/saints/dto.rs
+++ src/modules/saints/dto.rs
@@ -1,5 +1,6 @@
+use sqlx::FromRow;
+
 // use chrono::NaiveDate;
-use super::model::SaintRow;
 use serde::{Deserialize, Serialize};
 
 // Saints Paginated Response
@@ -11,7 +12,7 @@ pub struct SaintListResponse {
 }
 
 // Saint small struct for listing
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, FromRow)]
 pub struct SaintListItem {
     pub id: i32,
     pub slug: String,
@@ -19,22 +20,10 @@ pub struct SaintListItem {
     pub birth_year: Option<i16>,
     pub death_year: Option<i16>,
 }
-// Mapping from SaintRow to SaintListItem
-impl From<SaintRow> for SaintListItem {
-    fn from(row: SaintRow) -> Self {
-        Self {
-            id: row.id,
-            slug: row.slug,
-            default_name: row.default_name,
-            birth_year: row.birth_year,
-            death_year: row.death_year,
-        }
-    }
-}
 
 #[derive(Debug, Deserialize)]
 pub struct SaintQuery {
-    pub locale: Option<String>,
+    // pub locale: Option<String>,
     pub page: Option<i32>,
     pub per_page: Option<i32>,
 }
--- src/modules/saints/model.rs
+++ src/modules/saints/model.rs
@@ -1,21 +1,21 @@
-use chrono::{DateTime, Utc};
+// use chrono::{DateTime, Utc};
 
-#[derive(Debug, sqlx::FromRow)]
-pub struct SaintRow {
-    pub id: i32,
-    pub slug: String,
-    pub default_name: String,
-    pub birth_year: Option<i16>,
-    pub birth_month: Option<i16>,
-    pub birth_day: Option<i16>,
-    pub birth_is_approximate: bool,
-    pub death_year: Option<i16>,
-    pub death_month: Option<i16>,
-    pub death_day: Option<i16>,
-    pub death_is_approximate: bool,
-    pub century: Option<i16>,
-    pub place_of_birth_id: Option<i32>,
-    pub place_of_death_id: Option<i32>,
-    pub place_of_activity_id: Option<i32>,
-    pub created_at: DateTime<Utc>,
-}
+// #[derive(Debug, sqlx::FromRow)]
+// pub struct SaintRow {
+//     pub id: i32,
+//     pub slug: String,
+//     pub default_name: String,
+//     pub birth_year: Option<i16>,
+//     pub birth_month: Option<i16>,
+//     pub birth_day: Option<i16>,
+//     pub birth_is_approximate: bool,
+//     pub death_year: Option<i16>,
+//     pub death_month: Option<i16>,
+//     pub death_day: Option<i16>,
+//     pub death_is_approximate: bool,
+//     pub century: Option<i16>,
+//     pub place_of_birth_id: Option<i32>,
+//     pub place_of_death_id: Option<i32>,
+//     pub place_of_activity_id: Option<i32>,
+//     pub created_at: DateTime<Utc>,
+// }
