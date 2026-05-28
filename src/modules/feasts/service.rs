146c586a712296c70c780a8407aa63125ff96369
Cyrille Breuil <cybreuil@gmail.com>
2026-05-29 00:16:27 +0200

    Update service.rs

--- src/modules/feasts/service.rs
+++ src/modules/feasts/service.rs
@@ -1,4 +1,4 @@
-use chrono::{Datelike, FixedOffset, TimeZone, Utc};
+use chrono::{Datelike, FixedOffset, Utc};
 use sqlx::PgPool;
 
 use super::{dto::FeastListItem, repo};
@@ -22,7 +22,7 @@ pub async fn feast_the_day(
             let offset = offset.unwrap_or(0);
 
             let tz = FixedOffset::east_opt(offset * 60)
-                .ok_or_else(|| ApiError::BadRequest("Invalid offset".into()))?;
+                .ok_or_else(|| ApiError::UnprocessableEntity("Invalid offset".into()))?;
             let now = Utc::now().with_timezone(&tz);
             (now.month() as u8, now.day() as u8)
         }
@@ -35,27 +35,29 @@ fn parse_mm_dd(date: &str) -> Result<(u8, u8), ApiError> {
     let parts: Vec<&str> = date.split('-').collect();
 
     if parts.len() != 2 {
-        return Err(ApiError::BadRequest(
+        return Err(ApiError::UnprocessableEntity(
             "Invalid date format. Expected MM-DD.".into(),
         ));
     }
 
     let month = parts[0]
         .parse::<u8>()
-        .map_err(|_| ApiError::BadRequest("Invalid month.".into()))?;
+        .map_err(|_| ApiError::UnprocessableEntity("Invalid month.".into()))?;
 
     let day = parts[1]
         .parse::<u8>()
-        .map_err(|_| ApiError::BadRequest("Invalid day.".into()))?;
+        .map_err(|_| ApiError::UnprocessableEntity("Invalid day.".into()))?;
 
     if !(1..=12).contains(&month) {
-        return Err(ApiError::BadRequest(
+        return Err(ApiError::UnprocessableEntity(
             "Month must be between 1 and 12.".into(),
         ));
     }
 
     if !(1..=31).contains(&day) {
-        return Err(ApiError::BadRequest("Day must be between 1 and 31.".into()));
+        return Err(ApiError::UnprocessableEntity(
+            "Day must be between 1 and 31.".into(),
+        ));
     }
 
     Ok((month, day))
