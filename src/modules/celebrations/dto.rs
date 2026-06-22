f8e64fac47888685b176f1a1440b351aa07febdb
Cyrille Breuil <cybreuil@gmail.com>
2026-06-23 00:04:21 +0200

    wip dto for celebration

--- src/modules/celebrations/dto.rs
+++ src/modules/celebrations/dto.rs
@@ -6,4 +6,13 @@ pub struct Celebration {
     pub id: i32,
     pub color_id: Option<i32>,
     pub is_optional: bool,
+    pub rank_id: i32,
+    pub feast_id: i32,
+    pub date_kind: Option<String>,
+    pub month: Option<i16>,
+    pub day: Option<i16>,
+    pub movable_base: Option<String>,
+    pub movable_offset_days: Option<i16>,
+    pub notes: Option<String>,
+    pub observance_type: Option<String>,
 }
